dcbb339b5387bcd24b0f4bd20d3ab9e08a78aa81
Michael Zhao <michael2012zhao@hotmail.com>
2022-12-05 22:35:06 +0800

    Tree: Initial support
    Signed-off-by: Michael Zhao <michael2012zhao@hotmail.com>

---
+++ src/tree.rs
@@ -0,0 +1,30 @@
+// Copyright (c) 2022, Michael Zhao
+// SPDX-License-Identifier: MIT
+
+use crate::node::Node;
+
+pub struct Tree {
+    root: Node,
+}
+
+impl Tree {
+    pub fn new(root: Node) -> Self {
+        Tree { root }
+    }
+    pub fn to_dts(&self) -> String {
+        self.root.to_dts()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::node::Node;
+
+    #[test]
+    fn test_simple_tree() {
+        let root = Node::new("root");
+        let tree = Tree::new(root);
+        println!("{}", tree.to_dts());
+    }
+}
