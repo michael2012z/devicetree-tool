647539f56399515c45f3859fb4f047b9b4fe7687
Michael Zhao <michael2012zhao@hotmail.com>
2023-01-04 21:17:55 +0800

    Node: Add unit test
    Signed-off-by: Michael Zhao <michael2012zhao@hotmail.com>

--- src/node.rs
+++ src/node.rs
@@ -35,3 +35,53 @@ impl std::fmt::Display for Node {
         writeln!(f, "{s}")
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::attribute::Attribute;
+
+    #[test]
+    fn test_node_empty() {
+        let node = Node::new("node");
+        assert_eq!(node.attributes.len(), 0);
+        assert_eq!(node.sub_nodes.len(), 0);
+    }
+
+    #[test]
+    fn test_node_sub_nodes() {
+        let mut node = Node::new("node");
+        node.add_sub_node(Node::new("sub_node_0"));
+        node.add_sub_node(Node::new("sub_node_1"));
+        node.add_sub_node(Node::new("sub_node_2"));
+
+        let mut sub_node_3 = Node::new("sub_node_3");
+        sub_node_3.add_sub_node(Node::new("sub_node_30"));
+        sub_node_3.add_sub_node(Node::new("sub_node_31"));
+
+        assert_eq!(sub_node_3.sub_nodes.len(), 2);
+
+        node.add_sub_node(sub_node_3);
+        assert_eq!(node.sub_nodes.len(), 4);
+    }
+
+    #[test]
+    fn test_node_attributes() {
+        let mut node = Node::new("node");
+        node.add_attr(Attribute::new_empty("attr0"));
+        node.add_attr(Attribute::new_u32("attr1", 42));
+        assert_eq!(node.attributes.len(), 2);
+    }
+
+    #[test]
+    fn test_attribute_print() {
+        let mut node = Node::new("node");
+        node.add_attr(Attribute::new_u32("attr", 42));
+        let mut sub_node = Node::new("node");
+        sub_node.add_attr(Attribute::new_u32("attr", 12));
+        node.add_sub_node(sub_node);
+
+        let printing = format!("{}", node);
+        assert_eq!(&printing, "node {\n\tattr = <0x0 0x0 0x0 0x2a>;\n\n\tnode {\n\t\tattr = <0x0 0x0 0x0 0xc>;\n\t};\n};\n");
+    }
+}
