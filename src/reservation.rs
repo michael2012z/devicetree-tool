58901d6f33bd7d12ff2048e759940ba4075285bd
Michael Zhao <michael2012zhao@hotmail.com>
2023-02-15 08:47:14 +0800

    Documentation: Add doc comments for Reservation
    Add document comments for `Reservation`.
    
    Signed-off-by: Michael Zhao <michael2012zhao@hotmail.com>

--- src/reservation.rs
+++ src/reservation.rs
@@ -3,12 +3,24 @@
 
 use crate::dts_generator::DtsGenerator;
 
+/// A memory reservation block for reserving physical memory.
 #[derive(Copy, Clone)]
 pub struct Reservation {
     pub address: u64,
     pub length: u64,
 }
 
+/// Create a new memory reservation block with physical address and length
+///
+/// Example:
+///
+/// ```
+/// use devicetree_tool::reservation::Reservation;
+///
+/// let resv = Reservation::new(0, 0x1000);
+///
+/// assert_eq!(format!("{}", resv), "/memreserve/ 0x0000000000000000 0x0000000000001000;");
+/// ```
 impl Reservation {
     pub fn new(address: u64, length: u64) -> Self {
         Reservation { address, length }
@@ -16,6 +28,7 @@ impl Reservation {
 }
 
 impl std::fmt::Display for Reservation {
+    /// Print a `Property` in the format of DTS
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         let s = DtsGenerator::generate_reservation(self, 0);
         write!(f, "{s}")
