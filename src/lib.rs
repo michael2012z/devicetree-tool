f8c4cdc2620ecb19212ab0c809570491546dd34f
Michael Zhao <michael2012zhao@hotmail.com>
2023-02-19 17:49:53 +0800

    lib: Hide modes
    Signed-off-by: Michael Zhao <michael2012zhao@hotmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,18 @@
 // Copyright (c) 2023, Michael Zhao
 // SPDX-License-Identifier: MIT
 
-pub mod devicetree;
+mod devicetree;
 pub use devicetree::DeviceTree;
-pub mod dtb;
+mod dtb;
 pub use dtb::DtbHeader;
 mod dtb_generator;
 mod dtb_parser;
 mod dts_generator;
 mod dts_parser;
-pub mod node;
+mod node;
 pub use node::Node;
-pub mod property;
+mod property;
 pub use property::Property;
-pub mod reservation;
+mod reservation;
 pub use reservation::Reservation;
 mod utils;
